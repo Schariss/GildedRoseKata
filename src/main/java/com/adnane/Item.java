9fd4a511a48032434976e0371b68404390f350bf
Chahid Adnane <adnanechahid@gmail.com>
2022-08-30 00:43:43 +0200

    test(GildedRoseTest): quality degrades twice as fast when the sell by date has passed

--- src/main/java/com/adnane/Item.java
+++ src/main/java/com/adnane/Item.java
@@ -18,4 +18,31 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        // If the object is compared with itself then return true
+//        if (obj == this)
+//            return true;
+//        if(!this.name.equals(((Item) obj).name)
+//                || this.sellIn != ((Item) obj).sellIn
+//                || this.quality != ((Item) obj).quality
+//        )
+//            return false;
+//        if(obj == null)
+//            return false;
+//        if(obj.getClass() != this.getClass())
+//            return false;
+//        return true;
+//    }
+//
+//    @Override
+//    public int hashCode(){
+//        final int prime = 31;
+//        int result = 1;
+//        result = prime * result + ((name == null) ? 0 : name.hashCode());
+//        result = prime * result + quality;
+//        result = prime * result + sellIn;
+//        return result;
+//    }
 }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void qualityDegradesTwiceAsFastWhenSellInDatePass() {
+        Item[] items = new Item[] { new Item("foo", 0, 4) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.getItems()[0].name);
+
+        // Should implement equals and hashCode, since we are not allowed
+        // to modify the Item class, we are going to test each field apart
+        //assertEquals(new Item("foo", -1, 2), app.getItems()[0]);
+        Item item = new Item("foo", -1, 2);
+        assertEquals(item.name, app.getItems()[0].name);
+        assertEquals(item.quality, app.getItems()[0].quality);
+        assertEquals(item.sellIn, app.getItems()[0].sellIn);
     }
 
 }
