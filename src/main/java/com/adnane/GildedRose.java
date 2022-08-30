885abc9260c6dfafd63c9b99a658e92d5d2212a7
Chahid Adnane <adnanechahid@gmail.com>
2022-08-30 05:08:43 +0200

    feat(GildedRose): refactor code to multiple methods

--- src/main/java/com/adnane/GildedRose.java
+++ src/main/java/com/adnane/GildedRose.java
@@ -1,7 +1,15 @@
 package com.adnane;
 
+import java.util.function.Predicate;
+
 public class GildedRose {
-    private Item[] items;
+    private final Item[] items;
+    private final int MAX_QUALITY = 50;
+    private final int MIN_QUALITY = 0;
+    public static final String AGED_BRIE =  "Aged Brie";
+    public static final String BACKSTAGE =  "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS =  "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED =  "Conjured Mana Cake";
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -10,55 +18,57 @@ public class GildedRose {
     public Item[] getItems(){ return items; }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item :items) {
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            if(!item.name.equals(BACKSTAGE) && !item.name.equals(AGED_BRIE) && !item.name.equals(SULFURAS)){
+                handleQualityForConjuredAndNormalItems(item);
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            if(item.name.equals(AGED_BRIE))
+                handleQualityForAgedBrieItems(item);
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            if(item.name.equals(BACKSTAGE))
+                handleQualityForBackStagePassesItems(item);
         }
+    }
+
+    public Predicate<Item> isExpired = item -> item.sellIn < 1;
+
+    public void decreaseSellIn(Item item){
+        item.sellIn -= 1;
+    }
+
+    public void handleQuality(Item item, int by){
+        item.quality = item.quality + by;
+        if(item.quality > MAX_QUALITY)
+            item.quality = MAX_QUALITY;
+        else if(item.quality < MIN_QUALITY)
+            item.quality = MIN_QUALITY;
+    }
+
+    public void handleQualityForConjuredAndNormalItems(Item item){
+        int rate = item.name.equals(CONJURED)? -2: -1;
+        if(isExpired.test(item))
+            rate = rate*2;
+        handleQuality(item, rate);
+        decreaseSellIn(item);
+    }
+
+    public void handleQualityForAgedBrieItems(Item item){
+        int rate = isExpired.test(item)? 2: 1;
+        handleQuality(item, rate);
+        decreaseSellIn(item);
+    }
+
+    public void handleQualityForBackStagePassesItems(Item item){
+        handleQuality(item, 1);
+        if(item.sellIn <= 10){
+            handleQuality(item, 1);
+            if(item.sellIn <= 5)
+                handleQuality(item, 1);
+            if(isExpired.test(item))
+                handleQuality(item, -item.quality);
+        }
+        decreaseSellIn(item);
     }
 }
